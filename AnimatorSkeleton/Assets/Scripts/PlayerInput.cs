69466b5235056d5746c3f53f365b8de3d778073a
Caleb Kim <calebmk21@gmail.com>
2024-04-30 18:24:38 -0700

    finished animator skeleton

--- AnimatorSkeleton/Assets/Scripts/PlayerInput.cs
+++ AnimatorSkeleton/Assets/Scripts/PlayerInput.cs
@@ -14,22 +14,45 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpHeight = 9.5f;
     private float movement = 0f;
-    
+
+    private bool facingRight = true;
+    private Animator animator;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     void OnMove(InputValue value)
     {
         movement = value.Get<float>();
         Debug.Log(movement);
+        if (movement < 0 && facingRight == true)
+        {
+            Flip();
+        }
+
+        if (movement > 0 && facingRight == false)
+        {
+            Flip();
+        }
+        
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 
     void Update()
     {
         Move(movement);
+        animator.SetFloat("Speed", Mathf.Abs(speed * movement));
 
     }
     
@@ -50,6 +73,7 @@ public class PlayerInput : MonoBehaviour
     {
         Vector2 jumpAdder = new Vector2(rb.velocity.x, jumpHeight);
         rb.AddForce(jumpAdder, ForceMode2D.Impulse);
+        animator.SetBool("IsJumping", true);
     }
 
     
@@ -64,6 +88,7 @@ public class PlayerInput : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("IsJumping", false);
         }
     }
 
